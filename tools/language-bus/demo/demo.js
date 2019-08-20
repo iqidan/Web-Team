391c42ab651de6b2a943d3474b3050e9d5cd0ac8
iqidan <1506776365@qq.com>
2019-08-20 15:50:22 +0800

    运行测试的demo，完善

--- tools/language-bus/demo/demo.js
+++ tools/language-bus/demo/demo.js
@@ -1,14 +1,14 @@
 const SimpleBig = require('simplebig');
 const LanguageBus = require('../index');
 
-const lanBus = new LanguageBus({origin_path: 'zh_cn.json'});
+const lanBus = new LanguageBus({origin_path: './demo/zh_cn.json'});
 
 const targets = [
   {
-    path:'zh_tw.json',
+    path:'./demo/zh_tw.json',
     transfer: SimpleBig.s2t
   },
-  'en.json'
+  './demo/en.json'
 ];
 
 lanBus.writeFiles(targets);
